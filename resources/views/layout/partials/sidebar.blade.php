df1dc449d2b1608cae3b9c15779c2c70e96d21f9
skipper-09 <dxdskipper@gmail.com>
2024-11-09 16:50:40 +0700

    fix complic

--- resources/views/layout/partials/sidebar.blade.php
+++ resources/views/layout/partials/sidebar.blade.php
@@ -47,10 +47,9 @@
                     <ul class="sub-menu" aria-expanded="false">
                         <li><a href="ui-alerts.html">Items</a></li>
                         <li><a href="{{ route('itemtype') }}">Items Type</a></li>
-                        <li><a href="ui-alerts.html">Unit/Satuan</a></li>
-                        <li><a href="{{ route('company') }}">Company</a></li>
                         <li><a href="{{ route('unit') }}">Unit/Satuan</a></li>
-                        <li><a href="ui-buttons.html">Company</a></li>
+                        <li><a href="{{ route('company') }}">Company</a></li>
+                        
 
                     </ul>
                 </li>
