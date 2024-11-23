74a417d5e8ab00bddeff2295f01a9b70823d670d
skipper-09 <dxdskipper@gmail.com>
2024-11-23 11:37:19 +0700

    add migration vendor report

--- app/Models/ReportVendor.php
+++ app/Models/ReportVendor.php
@@ -6,13 +6,13 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
-class DetailItemProject extends Model
+class ReportVendor extends Model
 {
     use HasUuids;
     protected $primaryKey = 'id';
     public $incrementing = false;
     protected $keyType = 'string';
-    protected $fillable = ['detail_id','item_id','quantity','cost_material','cost_service'];
+    protected $fillable = ['project_id','vendor_id','tittle','description','image'];
     protected static function boot()
     {
         parent::boot();
@@ -22,12 +22,5 @@ class DetailItemProject extends Model
                 $model->id = (string) Str::uuid(); // Generate UUID
             }
         });
-    }
-
-    public function detail(){
-        return $this->belongsTo(DetailProject::class,'detail_id','id');
-    }
-    public function item(){
-        return $this->belongsTo(Item::class);
     }
 }
---
+++ database/migrations/2024_11_23_033711_create_report_vendors_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('report_vendors', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('project_id')->references('id')->on('projects')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignUuid('vendor_id')->references('id')->on('vendors')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('tittle');
+            $table->longText('description');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('report_vendors');
+    }
+};
