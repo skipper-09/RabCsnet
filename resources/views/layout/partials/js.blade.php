529ab6e8caadc63167de5cdeefc7f5147a171e0f
skipper-09 <dxdskipper@gmail.com>
2024-12-23 15:15:26 +0700

    fix log eror console

--- resources/views/layout/partials/js.blade.php
+++ resources/views/layout/partials/js.blade.php
@@ -7,7 +7,7 @@
 <script src="{{ asset('assets/libs/select2/js/select2.min.js') }}"></script>
 
 <!-- apexcharts -->
-<script src="{{ asset('assets/libs/apexcharts/apexcharts.min.js') }}"></script>
+{{-- <script src="{{ asset('assets/libs/apexcharts/apexcharts.min.js') }}"></script> --}}
 <script src="{{ asset('assets/libs/jquery-knob/jquery.knob.min.js') }}"></script> 
 
 
@@ -15,7 +15,7 @@
 <script src="{{ asset('assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
 <script src="{{ asset('assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-world-mill-en.js') }}"></script>
 
-<script src="{{ asset('assets/js/pages/dashboard.init.js') }}"></script>
+{{-- <script src="{{ asset('assets/js/pages/dashboard.init.js') }}"></script> --}}
 <script src="{{ asset('assets/libs/select2/js/select2.min.js') }}"></script>
 <script src="{{ asset('assets/js/app.js') }}"></script>
 <script src="{{ asset('assets/js/custom.js') }}"></script>
